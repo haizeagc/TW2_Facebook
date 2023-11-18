4bb6017cc7adf884419f3a493e4640a026654b60
haizeagc <116098300+haizeagc@users.noreply.github.com>
2023-11-18 13:19:53 +0100

    v3 facebook cosas añadidas

---
+++ src/sd/deusto/ingenieria/sd/tw2/methods/FacebookMethods.java
@@ -0,0 +1,12 @@
+package sd.deusto.ingenieria.sd.tw2.methods;
+
+public class FacebookMethods {
+	
+	public void register(String email, String password) {
+		
+	}
+	
+	public void loginService(String email, String password) {
+		
+	}
+}
