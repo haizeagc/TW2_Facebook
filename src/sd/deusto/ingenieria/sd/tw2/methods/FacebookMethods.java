8be8a84c7642c74c404ae23032056a873906a043
haizeagc <116098300+haizeagc@users.noreply.github.com>
2023-11-20 11:28:44 +0100

    v5
    No se muy bien si lo que he hecho tiene algún tipo de sentido.

--- src/sd/deusto/ingenieria/sd/tw2/methods/FacebookMethods.java
+++ src/sd/deusto/ingenieria/sd/tw2/methods/FacebookMethods.java
@@ -1,12 +1,53 @@
 package sd.deusto.ingenieria.sd.tw2.methods;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FacebookMethods {
 	
-	public void register(String email, String password) {
+	private ArrayList<Account> account = new ArrayList<>();
+	private HashMap<Email, String> map = new HashMap<>();
+	
+	public void registration(String email, String password) {
+		Account c1 = new Account();
+		boolean h = true;
 		
+		c1.setEmail(email);
+		c1.setPassword(password);
+
+		//change latter to sending the email to google or facebook for validation
+		for (Account account2 : account) {
+			if(account2.getName().equals(name)) {
+				h = false;
+				break;
+			}
+		}
+		if (h) {
+			account.add(c1);
+			return c1;
+		}else {
+			return null;
+		}
 	}
 	
-	public void loginService(String email, String password) {
-		
+	public Account login(String email, String password) {
+		Account account = new Account();
+		//just until the login with facebook is done
+		boolean b = false;
+		for (Account acaux : this.account) {
+			if (acaux.getEmail().equals(email)) {
+				//remove once loggin with facebook or google is done
+				if (acaux.checkPassword(password)) {
+					account.setEmail(email);
+					account.setPassword(password);
+					b = true;
+					break;
+				}
+			}
+		}
+		if (b) {
+			return account;
+		} else {
+			return null;		}
 	}
 }
